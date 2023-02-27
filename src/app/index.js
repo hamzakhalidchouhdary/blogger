a2cecfe6b844307470c487d6aa0275abb6375ddd
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-02-27 19:26:53 +0500

    create basic app

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const PORT = 8700;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`APP STARTED AT ${PORT}`);
+});
+
+module.export = app;
