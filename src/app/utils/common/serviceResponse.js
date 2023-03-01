75e9639f4b1a42d27e249ee2878c6b95dc24d6d7
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-01 18:59:18 +0500

    replace constant value

--- src/app/utils/common/serviceResponse.js
+++ src/app/utils/common/serviceResponse.js
@@ -1,7 +1,8 @@
 const HTTP_STATUS = require("../constants/httpStatus");
+const RESPONSE_TEXT = require("../constants/responeText");
 
 const errorResponse = function(res, {error, status}) {
-  res.status(status || HTTP_STATUS.INTERNAL_ERROR).send(error || 'internal error');
+  res.status(status || HTTP_STATUS.INTERNAL_ERROR).send(error || RESPONSE_TEXT.INTERNAL_ERROR);
 };
 
 module.exports = {
