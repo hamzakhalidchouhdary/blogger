71da6c5cb2e2b0596997591acc5e2bae626f7d2d
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-01 18:25:58 +0500

    update signup callback method

--- src/app/routers/v1/signup.js
+++ src/app/routers/v1/signup.js
@@ -1,10 +1,8 @@
 const HTTP_STATUS = require('../../utils/constants/httpStatus');
 const router = require('express').Router();
+const AuthService = require('../../apis/authService');
 
-router.post('/', (req, res) => {
-  res.status(HTTP_STATUS.OK).end();
-  return;
-})
+router.post('/', AuthService.signupNewUser)
 
 router.use('/', (req, res) => {
   res.status(HTTP_STATUS.NOT_ALLOWED).end();
