bec234a33b64bc26e4f5930d1fdce77262ac45f6
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-01 18:54:40 +0500

    add fall backs

--- src/app/utils/common/serviceResponse.js
+++ src/app/utils/common/serviceResponse.js
@@ -1,7 +1,7 @@
 const HTTP_STATUS = require("../constants/httpStatus");
 
 const errorResponse = function(res, {error, status}) {
-  res.status(status).send(error);
+  res.status(status || HTTP_STATUS.INTERNAL_ERROR).send(error || 'internal error');
 };
 
 module.exports = {
