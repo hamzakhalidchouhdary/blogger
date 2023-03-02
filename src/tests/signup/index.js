da833b63736d037477ac5e81739cf1ccbd575773
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-02 21:40:49 +0500

    update auth test cases

--- src/tests/signup/index.js
+++ src/tests/signup/index.js
@@ -7,30 +7,32 @@ chai.use(chaiHttp);
 chai.should();
 const request = chai.request;
 
-describe('Signup', function() {
-  it('should create new user', async function() {
-    const resp = await request(app)
-      .post('/api/v1/signup')
-      .send({});
-    resp.status.should.equal(HTTP_STATUS.CREATED);
-    resp.body.should.have.key('id');
-  });
-  it('should access signup:get route', async function() {
-    const resp = await request(app)
-      .get('/api/v1/signup')
-      .send({});
-    resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
-  });
-  it('should access signup:put route', async function() {
-    const resp = await request(app)
-      .put('/api/v1/signup')
-      .send({});
-    resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
-  });
-  it('should access signup:delete route', async function() {
-    const resp = await request(app)
-      .delete('/api/v1/signup')
-      .send({});
-    resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
+describe('auth', function() {
+  describe('Signup', function() {
+    it('should create new user', async function() {
+      const resp = await request(app)
+        .post('/api/v1/auth/signup')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.CREATED);
+      resp.body.should.have.key('id');
+    });
+    it('should access signup:get route', async function() {
+      const resp = await request(app)
+        .get('/api/v1/auth/signup')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
+    });
+    it('should access signup:put route', async function() {
+      const resp = await request(app)
+        .put('/api/v1/auth/signup')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
+    });
+    it('should access signup:delete route', async function() {
+      const resp = await request(app)
+        .delete('/api/v1/auth/signup')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
+    });
   });
 });
