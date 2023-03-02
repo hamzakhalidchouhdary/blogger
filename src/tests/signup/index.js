a304257690692230062554257a30211f5d7e7106
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-02 21:50:19 +0500

    add login test cases

--- src/tests/signup/index.js
+++ src/tests/signup/index.js
@@ -35,4 +35,31 @@ describe('auth', function() {
       resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
     });
   });
+  describe('Login', function() {
+    it('should login user', async function() {
+      const resp = await request(app)
+        .post('/api/v1/auth/login')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.OK);
+      resp.body.should.have.key('token');
+    });
+    it('should access login:get route', async function() {
+      const resp = await request(app)
+        .get('/api/v1/auth/login')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
+    });
+    it('should access login:put route', async function() {
+      const resp = await request(app)
+        .put('/api/v1/auth/login')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
+    });
+    it('should access login:delete route', async function() {
+      const resp = await request(app)
+        .delete('/api/v1/auth/login')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
+    });
+  });
 });
