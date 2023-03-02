4b4ab23705c5ab2677ff8c48ba7555bbab85ce82
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-02 21:55:07 +0500

    split signup and login test cases

--- src/tests/auth/login.js
+++ src/tests/auth/login.js
@@ -8,33 +8,6 @@ chai.should();
 const request = chai.request;
 
 describe('auth', function() {
-  describe('Signup', function() {
-    it('should create new user', async function() {
-      const resp = await request(app)
-        .post('/api/v1/auth/signup')
-        .send({});
-      resp.status.should.equal(HTTP_STATUS.CREATED);
-      resp.body.should.have.key('id');
-    });
-    it('should access signup:get route', async function() {
-      const resp = await request(app)
-        .get('/api/v1/auth/signup')
-        .send({});
-      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
-    });
-    it('should access signup:put route', async function() {
-      const resp = await request(app)
-        .put('/api/v1/auth/signup')
-        .send({});
-      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
-    });
-    it('should access signup:delete route', async function() {
-      const resp = await request(app)
-        .delete('/api/v1/auth/signup')
-        .send({});
-      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
-    });
-  });
   describe('Login', function() {
     it('should login user', async function() {
       const resp = await request(app)
---
+++ src/tests/auth/signup.js
@@ -0,0 +1,38 @@
+const app = require('../../app/index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const HTTP_STATUS = require('../../app/utils/constants/httpStatus');
+
+chai.use(chaiHttp);
+chai.should();
+const request = chai.request;
+
+describe('auth', function() {
+  describe('Signup', function() {
+    it('should create new user', async function() {
+      const resp = await request(app)
+        .post('/api/v1/auth/signup')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.CREATED);
+      resp.body.should.have.key('id');
+    });
+    it('should access signup:get route', async function() {
+      const resp = await request(app)
+        .get('/api/v1/auth/signup')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
+    });
+    it('should access signup:put route', async function() {
+      const resp = await request(app)
+        .put('/api/v1/auth/signup')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
+    });
+    it('should access signup:delete route', async function() {
+      const resp = await request(app)
+        .delete('/api/v1/auth/signup')
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.NOT_ALLOWED);
+    });
+  });
+});
