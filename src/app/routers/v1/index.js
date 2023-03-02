722df89dd0e9d192471fa71f16013d4ee8b28052
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-02 21:51:08 +0500

    add login routing

--- src/app/routers/v1/index.js
+++ src/app/routers/v1/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const signup = require('./signup')
+const login = require('./login');
 
 router.use('/auth/signup', signup);
+router.use('/auth/login', login);
 
 module.exports = router;
