8bf1acc092e05782d65cf0da45076c7e43ee9a08
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-06 21:25:07 +0500

    add test cases for article posts

---
+++ src/tests/articles/posts.js
@@ -0,0 +1,41 @@
+const app = require('../../app/index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const HTTP_STATUS = require('../../app/utils/constants/httpStatus');
+
+chai.use(chaiHttp);
+chai.should();
+const request = chai.request;
+
+describe.only('Article Posts', function() {
+  it('should allow user to create new post', async function() {
+    const resp = await request(app)
+      .post('/api/v1/article')
+      .send({});
+    resp.status.should.equal(HTTP_STATUS.CREATED);
+  });
+  it('should allow user to edit a post', async function() {
+    const resp = await request(app)
+      .put('/api/v1/article')
+      .send({});
+    resp.status.should.equal(HTTP_STATUS.OK);
+  });
+  it('should allow user to delete a post', async function() {
+    const resp = await request(app)
+      .delete('/api/v1/article')
+      .send({});
+    resp.status.should.equal(HTTP_STATUS.OK);
+  });
+  it('should allow user to fetch all post', async function() {
+    const resp = await request(app)
+      .get('/api/v1/article')
+      .send({});
+    resp.status.should.equal(HTTP_STATUS.OK);
+  });
+  it('should allow user to fetch post by id', async function() {
+    const resp = await request(app)
+      .get('/api/v1/article/1')
+      .send({});
+    resp.status.should.equal(HTTP_STATUS.OK);
+  });
+});
