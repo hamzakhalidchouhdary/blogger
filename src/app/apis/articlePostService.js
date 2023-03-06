31dbe93a0bf66615b7c994fe4c4afcb0fcf71636
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-06 21:25:50 +0500

    add article post controller methods

---
+++ src/app/apis/articlePostService.js
@@ -0,0 +1,55 @@
+const ServiceResponse = require("../utils/common/serviceResponse");
+const HTTP_STATUS = require("../utils/constants/httpStatus");
+
+const createPost = function(req, res) {
+  try {
+    res.status(HTTP_STATUS.CREATED).json({});
+    return;
+  } catch (err) {
+    ServiceResponse.error(res, {});
+  };
+};
+
+const updatePost = function(req, res) {
+  try {
+    res.status(HTTP_STATUS.OK).json({});
+    return;
+  } catch (err) {
+    ServiceResponse.error(res, {});
+  };
+};
+
+const deletePost = function(req, res) {
+  try {
+    res.status(HTTP_STATUS.OK).json({});
+    return;
+  } catch (err) {
+    ServiceResponse.error(res, {});
+  };
+};
+
+const getPosts = function(req, res) {
+  try {
+    res.status(HTTP_STATUS.OK).json({});
+    return;
+  } catch (err) {
+    ServiceResponse.error(res, {});
+  };
+};
+
+const getPost = function(req, res) {
+  try {
+    res.status(HTTP_STATUS.OK).json({});
+    return;
+  } catch (err) {
+    ServiceResponse.error(res, {});
+  };
+};
+
+module.exports = {
+  createPost,
+  updatePost,
+  deletePost,
+  getPost,
+  getPosts
+};
