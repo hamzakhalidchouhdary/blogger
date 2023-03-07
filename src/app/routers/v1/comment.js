33675d977a11ced7abc64500542b1b5cfa6631df
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-07 19:02:25 +0500

    add routes for articleComments

---
+++ src/app/routers/v1/comment.js
@@ -0,0 +1,10 @@
+const HTTP_STATUS = require('../../utils/constants/httpStatus');
+const router = require('express').Router();
+const ArticleCommentService = require('../../apis/articleCommentSerivce');
+
+router.post('/', ArticleCommentService.createComment);
+router.put('/', ArticleCommentService.updateComment);
+router.get('/', ArticleCommentService.getComment);
+router.delete('/', ArticleCommentService.deleteComment);
+
+module.exports = router;
--- src/app/routers/v1/index.js
+++ src/app/routers/v1/index.js
@@ -4,11 +4,13 @@ const login = require('./login');
 const manageUser = require('./manageUser');
 const manageProfile = require('./manageProfile');
 const articles = require('./article');
+const comment = require('./comment');
 
 router.use('/auth/signup', signup);
 router.use('/auth/login', login);
 router.use('/user/manage', manageUser);
 router.use('/user/profile', manageProfile);
 router.use('/article', articles);
+router.use('/article/:id/comment', comment);
 
 module.exports = router;
