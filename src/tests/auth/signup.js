34dc6263e4a06e5a6c64493e2ad186f46065ff6e
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-09 20:24:26 +0500

    update signup/post test case

--- src/tests/auth/signup.js
+++ src/tests/auth/signup.js
@@ -2,6 +2,7 @@ const app = require('../../app/index');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const HTTP_STATUS = require('../../app/utils/constants/httpStatus');
+const UserModel = require('../../app/models').User;
 
 chai.use(chaiHttp);
 chai.should();
@@ -10,11 +11,21 @@ const request = chai.request;
 describe('auth', function() {
   describe('Signup', function() {
     it('should create new user', async function() {
+      const payload = {
+        name: 'test user'
+      };
       const resp = await request(app)
         .post('/api/v1/auth/signup')
-        .send({});
+        .send(payload);
       resp.status.should.equal(HTTP_STATUS.CREATED);
       resp.body.should.have.key('id');
+      const latestUser = await UserModel.findOne({
+        limit: 1,
+        where: {},
+        order: [ [ 'createdAt', 'DESC' ]]
+      });
+      resp.body.id.should.equal(latestUser.id);
+      latestUser.name.should.equal(payload.name);
     });
     it('should access signup:get route', async function() {
       const resp = await request(app)
