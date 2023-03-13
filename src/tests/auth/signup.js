a44bb6e8d53349162bba66822e638c2bb2033786
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-13 22:29:32 +0500

    update auth test cases

--- src/tests/auth/signup.js
+++ src/tests/auth/signup.js
@@ -3,19 +3,22 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const HTTP_STATUS = require('../../app/utils/constants/httpStatus');
 const UserModel = require('../../app/models').User;
+const { faker } = require('@faker-js/faker');
 
 chai.use(chaiHttp);
 chai.should();
 const request = chai.request;
 
 describe('auth', function() {
-  describe('Signup', function() {
-    it('should create new user', async function() {
+  describe.only('Signup', function() {
+    it('should create new admin user', async function() {
+      
       const payload = {
-        firstName: 'test',
-        lastName: 'user',
-        username: 'abc12',
-        hashedPassword: '1234'
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        username: faker.internet.userName(),
+        hashedPassword: faker.internet.password(),
+        role: 'admin'
       };
       const resp = await request(app)
         .post('/api/v1/auth/signup')
@@ -27,6 +30,48 @@ describe('auth', function() {
       latestUser.firstName.should.equal(payload.firstName);
       latestUser.lastName.should.equal(payload.lastName);
       latestUser.username.should.equal(payload.username);
+      latestUser.role.should.equal(payload.role);
+    });
+    it('should create new manager user', async function() {
+      const a = 0;
+      const payload = {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        username: faker.internet.userName(),
+        hashedPassword: faker.internet.password(),
+        role: 'manager'
+      };
+      const resp = await request(app)
+        .post('/api/v1/auth/signup')
+        .send(payload);
+      resp.status.should.equal(HTTP_STATUS.CREATED);
+      resp.body.should.have.key('id');
+      const latestUser = await UserModel.findLatest();
+      resp.body.id.should.equal(latestUser.id);
+      latestUser.firstName.should.equal(payload.firstName);
+      latestUser.lastName.should.equal(payload.lastName);
+      latestUser.username.should.equal(payload.username);
+      latestUser.role.should.equal(payload.role);
+    });
+    it('should create new reader user', async function() {
+      const payload = {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        username: faker.internet.userName(),
+        hashedPassword: faker.internet.password(),
+        role: 'reader'
+      };
+      const resp = await request(app)
+        .post('/api/v1/auth/signup')
+        .send(payload);
+      resp.status.should.equal(HTTP_STATUS.CREATED);
+      resp.body.should.have.key('id');
+      const latestUser = await UserModel.findLatest();
+      resp.body.id.should.equal(latestUser.id);
+      latestUser.firstName.should.equal(payload.firstName);
+      latestUser.lastName.should.equal(payload.lastName);
+      latestUser.username.should.equal(payload.username);
+      latestUser.role.should.equal(payload.role);
     });
     it('should access signup:get route', async function() {
       const userCountBefore = await UserModel.count();
