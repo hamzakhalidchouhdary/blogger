3a8208b3dfc9c0500879e2aa6fb543f91f20a9f8
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-15 19:59:17 +0500

    define jwt util methods

--- src/utils/common/auth.js
+++ src/utils/common/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const generateHashedPassword = async function(plainString) {
   try {
@@ -12,7 +13,28 @@ const compareHashedPassword = async function(plainPassword, hashedPassword) {
   return bcrypt.compare(plainPassword, hashedPassword);
 }
 
+const generateJWT = async function(payload) {
+  try {
+    return jwt.sign(payload, '12345');
+  } catch (err) {
+    throw err;
+  }
+}
+
+const verifyJWT = async function(token) {
+  try {
+    return jwt.verify(token, '12345', function(err, payload) {
+      if (err) throw err;
+      return payload;
+    });
+  } catch (err) {
+    throw err;
+  }
+}
+
 module.exports = {
   generateHashedPassword,
-  compareHashedPassword
+  compareHashedPassword,
+  generateJWT,
+  verifyJWT
 }
