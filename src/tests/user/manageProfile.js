7e89766f052f9b066dcddd9a3ae2e67071f1bccd
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-20 22:33:09 +0500

    update test cases

--- src/tests/user/manageProfile.js
+++ src/tests/user/manageProfile.js
@@ -2,15 +2,27 @@ const app = require('../../app/index');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const HTTP_STATUS = require('../../utils/constants/httpStatus');
+const UserFixture = require('../fixtures/user');
 
 chai.use(chaiHttp);
 chai.should();
 const request = chai.request;
 
 describe('Manage Profile', function() {
+  beforeEach(async function() {
+    this.user = await UserFixture.createUser({hashedPassword: '1234'});
+    const resp = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        username: this.user.username,
+        password: '1234'
+      });
+    this.token = resp.body.token;
+  })
   it('should allow to create profile', async function() {
     const resp = await request(app)
       .post('/api/v1/user/profile')
+      .set({ Authorization: `Bearer ${this.token}` })
       .send({});
     resp.status.should.equal(HTTP_STATUS.CREATED);
     resp.body.should.empty;
