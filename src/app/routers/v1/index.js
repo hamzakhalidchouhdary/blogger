b71f195874e6614e92826fd8da85a71a2935d084
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-20 22:32:27 +0500

    add user authorization middleware

--- src/app/routers/v1/index.js
+++ src/app/routers/v1/index.js
@@ -5,9 +5,26 @@ const manageUser = require('./manageUser');
 const manageProfile = require('./manageProfile');
 const articles = require('./article');
 const comment = require('./comment');
+const { verifyJWT } = require('../../../utils/common/auth');
+const { getUser } = require('../../modules/user');
+
+const authorizeUser = function(req, res, next) {
+  const token = req.headers.authorization.split(' ')[1];
+  verifyJWT(token)
+    .then(({userId}) => {
+      getUser(userId).then(user => {
+        req.user = user
+        next();
+      })
+    })
+    .catch(err => {
+      res.status(500).end();
+  });
+}
 
 router.use('/auth/signup', signup);
 router.use('/auth/login', login);
+router.use(authorizeUser);
 router.use('/user/manage', manageUser);
 router.use('/user/profile', manageProfile);
 router.use('/article', articles);
