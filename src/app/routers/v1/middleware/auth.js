5aaaa696d336a9539a64f2221d2d3ee48b67c7e7
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-21 22:30:04 +0500

    add header validation

--- src/app/routers/v1/middleware/auth.js
+++ src/app/routers/v1/middleware/auth.js
@@ -1,14 +1,17 @@
 const { verifyJWT } = require("../../../../utils/common/auth");
+const HttpStatus = require("../../../../utils/constants/httpStatus");
 const UserModule = require("../../../modules/user");
 
 const authorizeUser = async function(req, res, next) {
   try{
+    if (!req.headers.authorization) throw new Error('token not found');
     const token = req.headers.authorization.split(' ')[1];
+    if (!token) throw new Error('token not defined');
     const {userId} = await verifyJWT(token);
     const user = await UserModule.getUser(userId);
     next();
   } catch(err) {
-    res.status(500).end();
+    res.status(HttpStatus.UNAUTHORIZED).end(err.message);
   }
 }
 
