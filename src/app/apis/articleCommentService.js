414f1f808d990a1e804c60a9bf9a8125fe11b414
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-23 11:10:26 +0500

    update article comment service

--- src/app/apis/articleCommentService.js
+++ src/app/apis/articleCommentService.js
@@ -3,37 +3,44 @@ const HTTP_STATUS = require("../../utils/constants/httpStatus");
 
 const createComment = function(req, res) {
   try {
+    const {user} = req;
+    user.createComment();
     res.status(HTTP_STATUS.CREATED).json({});
     return;
   } catch (err) {
-    ServiceResponse.error(res, {});
+    ServiceResponse.error(res, err);
   };
 };
 
 const updateComment = function(req, res) {
   try {
+    const {user} = req;
+    user.updateComment();
     res.status(HTTP_STATUS.OK).json({});
     return;
   } catch (err) {
-    ServiceResponse.error(res, {});
+    ServiceResponse.error(res, err);
   };
 };
 
 const deleteComment = function(req, res) {
   try {
+    const {user} = req;
+    user.deleteComment();
     res.status(HTTP_STATUS.OK).json({});
     return;
   } catch (err) {
-    ServiceResponse.error(res, {});
+    ServiceResponse.error(res, err);
   };
 };
 
 const getComment = function(req, res) {
   try {
+    const {user} = req;
     res.status(HTTP_STATUS.OK).json({});
     return;
   } catch (err) {
-    ServiceResponse.error(res, {});
+    ServiceResponse.error(res, err);
   };
 };
 
