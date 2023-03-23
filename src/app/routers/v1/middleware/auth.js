99764897d670663a0f4cced2ec18ebaa2267e15d
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-23 11:43:58 +0500

    update user auth middle ware

--- src/app/routers/v1/middleware/auth.js
+++ src/app/routers/v1/middleware/auth.js
@@ -1,13 +1,13 @@
 const { verifyJWT } = require("../../../../utils/common/auth");
 const ServiceResponse = require("../../../../utils/common/serviceResponse");
-const HttpStatus = require("../../../../utils/constants/httpStatus");
+const HTTP_STATUS = require("../../../../utils/constants/httpStatus");
 const UserModule = require("../../../modules/user");
 
 const authorizeUser = async function(req, res, next) {
   try{
-    if (!req.headers.authorization) throw new Error('token not found');
+    if (!req.headers.authorization) throw Object({message: 'token not found', status: HTTP_STATUS.UNAUTHORIZED});
     const token = req.headers.authorization.split(' ')[1];
-    if (!token) throw new Error('token not defined');
+    if (!token) throw Object({message: 'token is empty', status: HTTP_STATUS.UNAUTHORIZED});
     const {userId} = await verifyJWT(token);
     req.user = await UserModule.getUser(userId);
     next();
