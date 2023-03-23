00fd9f5918f203551c2c5af3a424fe85bf3bf04e
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-23 11:21:55 +0500

    update service response error method

--- src/utils/common/serviceResponse.js
+++ src/utils/common/serviceResponse.js
@@ -1,8 +1,8 @@
 const HTTP_STATUS = require("../constants/httpStatus");
 const RESPONSE_TEXT = require("../constants/responseText");
 
-const errorResponse = function(res, {error, status}) {
-  res.status(status || HTTP_STATUS.INTERNAL_ERROR).send(error || RESPONSE_TEXT.INTERNAL_ERROR);
+const errorResponse = function(res, {message, status}) {
+  res.status(status || HTTP_STATUS.INTERNAL_ERROR).send(message || RESPONSE_TEXT.INTERNAL_ERROR);
 };
 
 module.exports = {
