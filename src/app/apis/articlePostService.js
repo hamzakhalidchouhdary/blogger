235c7d79df9f7b8747c9d9ee9ad39b8c780e0fe0
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-23 10:48:51 +0500

    update articalPost service

--- src/app/apis/articlePostService.js
+++ src/app/apis/articlePostService.js
@@ -3,46 +3,54 @@ const HTTP_STATUS = require("../../utils/constants/httpStatus");
 
 const createPost = function(req, res) {
   try {
+    const {user} = req;
+    user.createArticle();
     res.status(HTTP_STATUS.CREATED).json({});
     return;
   } catch (err) {
-    ServiceResponse.error(res, {});
+    ServiceResponse.error(res, err);
   };
 };
 
 const updatePost = function(req, res) {
   try {
+    const {user} = req;
+    user.updateArticle();
     res.status(HTTP_STATUS.OK).json({});
     return;
   } catch (err) {
-    ServiceResponse.error(res, {});
+    ServiceResponse.error(res, err);
   };
 };
 
 const deletePost = function(req, res) {
   try {
+    const {user} = req;
+    user.deleteArticle();
     res.status(HTTP_STATUS.OK).json({});
     return;
   } catch (err) {
-    ServiceResponse.error(res, {});
+    ServiceResponse.error(res, err);
   };
 };
 
 const getPosts = function(req, res) {
   try {
+    const {user} = req;
     res.status(HTTP_STATUS.OK).json({});
     return;
   } catch (err) {
-    ServiceResponse.error(res, {});
+    ServiceResponse.error(res, err);
   };
 };
 
 const getPost = function(req, res) {
   try {
+    const {user} = req;
     res.status(HTTP_STATUS.OK).json({});
     return;
   } catch (err) {
-    ServiceResponse.error(res, {});
+    ServiceResponse.error(res, err);
   };
 };
 
