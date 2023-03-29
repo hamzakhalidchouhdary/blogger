e612261d606802d0716026b558bee33ff59aed2e
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-03-29 20:19:48 +0500

    add `article fixtures` file

---
+++ src/tests/fixtures/article.js
@@ -0,0 +1,34 @@
+const ArticleModel = require('../../app/models').Article;
+const { faker } = require('@faker-js/faker');
+
+const getArticleDetails = function(article) {
+  return {
+    title: article.title || faker.random.words(5),
+    content: article.content || faker.random.words(20)
+  }
+}
+
+const createArticle = async function(article = {}, userId) {
+  try {
+    if (!userId) throw Object({message: 'userId is required'});
+    const articleDetails = getArticleDetails(article);
+    articleDetails.createdBy = articleDetails.updatedBy = userId;
+    return ArticleModel.new(articleDetails);
+  } catch (err) {
+    throw err;
+  }
+}
+
+const getLatestCreatedArticle = async function() {
+  return ArticleModel.findLatest();
+}
+
+const findArticleById = async function(id) {
+  return ArticleModel.findById(id);
+}
+
+module.exports = {
+  createArticle,
+  getLatestCreatedArticle,
+  findArticleById
+}
