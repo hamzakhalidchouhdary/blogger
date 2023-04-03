a5f572ebfb49c9c826ef665f142929e4c2848d27
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-04-03 21:16:03 +0500

    update comment router: merge params

--- src/app/routers/v1/comment.js
+++ src/app/routers/v1/comment.js
@@ -1,5 +1,5 @@
 const HTTP_STATUS = require('../../../utils/constants/httpStatus');
-const router = require('express').Router();
+const router = require('express').Router({mergeParams: true});
 const ArticleCommentService = require('../../apis/articleCommentService');
 
 router.post('/', ArticleCommentService.createComment);
