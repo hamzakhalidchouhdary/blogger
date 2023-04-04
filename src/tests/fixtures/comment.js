3f079d987168ae8a1148e598189066117dcd17a2
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-04-04 20:01:23 +0500

    add new comment fixture methods

--- src/tests/fixtures/comment.js
+++ src/tests/fixtures/comment.js
@@ -1,9 +1,28 @@
 const CommentModel = require('../../app/models').Comment;
+const { faker } = require('@faker-js/faker');
+const _ = require('lodash');
 
 const getCommentCount = async function (articleId) {
   return CommentModel.count({ where: { articleId } });
 };
 
+const createComment = async function (content = '', articleId = null, userId = null) {
+  if (_.isNull(articleId)) throw Object({ message: 'article id is missing' })
+  if (_.isNull(userId)) throw Object({ message: 'user id is missing' })
+  if (_.isEmpty(content)) {
+    content = faker.random.words(7);
+  }
+  const commentDetails = { content, articleId, createdBy: userId, updatedBy: userId };
+  return CommentModel.new(commentDetails);
+}
+
+const getCommentById = async function (commentId = null) {
+  if (_.isNull(commentId)) throw Object({ message: 'comment id is missing' })
+  return CommentModel.getById(commentId);
+}
+
 module.exports = {
-  getCommentCount
+  getCommentCount,
+  createComment,
+  getCommentById
 }
