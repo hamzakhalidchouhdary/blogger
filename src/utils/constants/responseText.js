547fe97ac16a83ef383743eed2e6d70d0b0f52bf
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-04-06 23:44:34 +0500

    add error text constants

--- src/utils/constants/responseText.js
+++ src/utils/constants/responseText.js
@@ -1,3 +1,6 @@
 module.exports = {
-  INTERNAL_ERROR: 'internal error'
+  INTERNAL_ERROR: 'internal error',
+  EMPTY: 'unexpected empty value',
+  NOT_OBJECT: 'a object is expected',
+  INVALID_NUM: 'invalid number value'
 }
