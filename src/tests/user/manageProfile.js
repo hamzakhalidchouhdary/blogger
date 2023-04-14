1bd3c8816e8cbd226193120f91d72cdfb28d9845
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-04-14 19:53:35 +0500

    wrap test in `admin role` block and set role as `admin`

--- src/tests/user/manageProfile.js
+++ src/tests/user/manageProfile.js
@@ -4,46 +4,49 @@ const chaiHttp = require("chai-http");
 const HTTP_STATUS = require("../../utils/constants/httpStatus");
 const UserFixture = require("../fixtures/user");
 const { generateJWT } = require("../../utils/common/auth");
+const USER_ROLES = require("../../utils/constants/userRoles");
 
 chai.use(chaiHttp);
 chai.should();
 const request = chai.request;
 
 describe("Manage Profile", function () {
-  beforeEach(async function () {
-    this.user = await UserFixture.createUser({});
-    this.token = await generateJWT({ userId: this.user.id });
-  });
-  it("should allow to create profile", async function () {
-    const resp = await request(app)
-      .post("/api/v1/user/profile")
-      .set({ Authorization: `Bearer ${this.token}` })
-      .send({});
-    resp.status.should.equal(HTTP_STATUS.CREATED);
-    resp.body.should.empty;
-  });
-  it("should allow to update profile", async function () {
-    const resp = await request(app)
-      .put("/api/v1/user/profile")
-      .set({ Authorization: `Bearer ${this.token}` })
-      .send({});
-    resp.status.should.equal(HTTP_STATUS.OK);
-    resp.body.should.empty;
-  });
-  it("should allow to delete profile", async function () {
-    const resp = await request(app)
-      .delete("/api/v1/user/profile")
-      .set({ Authorization: `Bearer ${this.token}` })
-      .send({});
-    resp.status.should.equal(HTTP_STATUS.OK);
-    resp.body.should.empty;
-  });
-  it("should allow to get profile", async function () {
-    const resp = await request(app)
-      .get("/api/v1/user/profile")
-      .set({ Authorization: `Bearer ${this.token}` })
-      .send({});
-    resp.status.should.equal(HTTP_STATUS.OK);
-    resp.body.should.empty;
+  describe("Admin Role", function () {
+    beforeEach(async function () {
+      this.user = await UserFixture.createUser({role: USER_ROLES.ADMIN});
+      this.token = await generateJWT({ userId: this.user.id });
+    });
+    it("should allow to create profile", async function () {
+      const resp = await request(app)
+        .post("/api/v1/user/profile")
+        .set({ Authorization: `Bearer ${this.token}` })
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.CREATED);
+      resp.body.should.empty;
+    });
+    it("should allow to update profile", async function () {
+      const resp = await request(app)
+        .put("/api/v1/user/profile")
+        .set({ Authorization: `Bearer ${this.token}` })
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.OK);
+      resp.body.should.empty;
+    });
+    it("should allow to delete profile", async function () {
+      const resp = await request(app)
+        .delete("/api/v1/user/profile")
+        .set({ Authorization: `Bearer ${this.token}` })
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.OK);
+      resp.body.should.empty;
+    });
+    it("should allow to get profile", async function () {
+      const resp = await request(app)
+        .get("/api/v1/user/profile")
+        .set({ Authorization: `Bearer ${this.token}` })
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.OK);
+      resp.body.should.empty;
+    });
   });
 });
