5baf62f96102b439379fc71751a61ca02ea92590
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-04-21 11:34:47 +0500

    fix formatting

--- src/app/routers/v1/middleware/validations/articles.js
+++ src/app/routers/v1/middleware/validations/articles.js
@@ -1,28 +1,36 @@
-const { validationResult, body } = require('express-validator');
-const serviceResponse = require('../../../../../utils/common/serviceResponse');
-const HTTP_STATUS = require('../../../../../utils/constants/httpStatus');
+const { validationResult, body } = require("express-validator");
+const serviceResponse = require("../../../../../utils/common/serviceResponse");
+const HTTP_STATUS = require("../../../../../utils/constants/httpStatus");
 
 const newArticle = async function (req, res, next) {
   try {
     const validationRules = [
-      body('title')
-      .exists({checkFalsy: true, checkNull: true})
-      .withMessage('Article title is required')
-      .trim().isEmpty().withMessage('Article title can not be empty'),
-      body('content')
-      .exists().withMessage('Article content is required')
-      .trim().isEmpty().withMessage('Article content can not be empty')
-    ]
-    await Promise.all(validationRules.map(rule => rule.run(req)));
+      body("title")
+        .exists({ checkFalsy: true, checkNull: true })
+        .withMessage("Article title is required")
+        .trim()
+        .isEmpty()
+        .withMessage("Article title can not be empty"),
+      body("content")
+        .exists()
+        .withMessage("Article content is required")
+        .trim()
+        .isEmpty()
+        .withMessage("Article content can not be empty"),
+    ];
+    await Promise.all(validationRules.map((rule) => rule.run(req)));
     const errors = validationResult(req);
     if (errors.isEmpty()) return next();
     throw errors.array();
   } catch (err) {
-    serviceResponse.error(res, {message: err, status: HTTP_STATUS.BAD_REQUEST});
+    serviceResponse.error(res, {
+      message: err,
+      status: HTTP_STATUS.BAD_REQUEST,
+    });
     return;
   }
-}
-  
+};
+
 module.exports = {
   newArticle,
 };
