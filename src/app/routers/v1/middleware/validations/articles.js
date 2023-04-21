7773192f8abff8fffa658fb77caa8aab1f306d4c
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-04-21 11:13:59 +0500

    add new validation method using `express-validation`

--- src/app/routers/v1/middleware/validations/articles.js
+++ src/app/routers/v1/middleware/validations/articles.js
@@ -1,6 +1,9 @@
 const RouteValidator = require('express-route-validator')
+const { validationResult, ValidationChain, body } = require('express-validator');
+const serviceResponse = require('../../../../../utils/common/serviceResponse');
+const HTTP_STATUS = require('../../../../../utils/constants/httpStatus');
 
-const newArticle = RouteValidator.validate({
+const newArticle_ = RouteValidator.validate({
   body: {
     title: {isRequired: true, message: 'Article title is required'},
     content: {isRequired: true, message: 'Article content is required'}
@@ -14,6 +17,27 @@ const newArticle = RouteValidator.validate({
   }
 });
 
+const newArticle = async function (req, res, next) {
+  try {
+    const validationRules = [
+      body('title')
+      .exists({checkFalsy: true, checkNull: true})
+      .withMessage('Article title is required')
+      .trim().isEmpty().withMessage('Article title can not be empty'),
+      body('content')
+      .exists().withMessage('Article content is required')
+      .trim().isEmpty().withMessage('Article content can not be empty')
+    ]
+    await Promise.all(validationRules.map(rule => rule.run(req)));
+    const errors = validationResult(req);
+    if (errors.isEmpty()) return next();
+    throw errors.array();
+  } catch (err) {
+    serviceResponse.error(res, {message: err, status: HTTP_STATUS.BAD_REQUEST});
+    return;
+  }
+}
+  
 module.exports = {
   newArticle,
 };
