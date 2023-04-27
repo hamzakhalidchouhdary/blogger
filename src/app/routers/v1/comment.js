fa70555a69bf772a219bf739c7375068fee3af9d
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-04-27 21:33:12 +0500

    add route validations

--- src/app/routers/v1/comment.js
+++ src/app/routers/v1/comment.js
@@ -1,8 +1,9 @@
 const HTTP_STATUS = require("../../../utils/constants/httpStatus");
 const router = require("express").Router({ mergeParams: true });
 const ArticleCommentService = require("../../apis/articleCommentService");
+const CommentValidation = require("./middleware/validations/comments");
 
-router.post("/", ArticleCommentService.createComment);
+router.post("/", CommentValidation.newComment, ArticleCommentService.createComment);
 router.put("/:id", ArticleCommentService.updateComment);
 router.get("/", ArticleCommentService.getComment);
 router.get("/list", ArticleCommentService.getCommentList);
---
+++ src/app/routers/v1/middleware/validations/comments.js
@@ -0,0 +1,28 @@
+const { body, validationResult } = require("express-validator");
+const serviceResponse = require("../../../../../utils/common/serviceResponse");
+const HTTP_STATUS = require("../../../../../utils/constants/httpStatus");
+
+const newComment = async function (req, res, next) {
+  try {
+    const validationRules = [
+      body("content")
+        .exists()
+        .withMessage("comment content is required")
+        .notEmpty()
+        .withMessage("comment content can not empty"),
+    ];
+    await Promise.all(validationRules.map(rule => rule.run(req)));
+    const errors = validationResult(req);
+    if (errors.isEmpty()) return next();
+    throw errors.array();
+  } catch (err) {
+    serviceResponse.error(res, {
+      message: err,
+      status: HTTP_STATUS.BAD_REQUEST,
+    });
+  }
+};
+
+module.exports = {
+  newComment,
+};
