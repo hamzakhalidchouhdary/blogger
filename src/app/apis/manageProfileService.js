0eaeee6aca330574fa5d63eb25736b85d1011af6
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-07-26 00:03:52 +0500

    updated error handling in `user/profile/*`

--- src/app/apis/manageProfileService.js
+++ src/app/apis/manageProfileService.js
@@ -1,45 +1,41 @@
 const ServiceResponse = require("../../utils/common/serviceResponse");
 const HTTP_STATUS = require("../../utils/constants/httpStatus");
 
-const createProfile = function (req, res) {
+const createProfile = function (req, res, err) {
   try {
     res.status(HTTP_STATUS.CREATED).end();
     return;
   } catch (err) {
-    ServiceResponse.error(res, err);
-    return;
+    return next(err);
   }
 };
 
-const updateProfile = async function (req, res) {
+const updateProfile = async function (req, res, next) {
   try {
     const { user, body: payload } = req;
     const updatedProfile = await user.updateProfile(payload);
     res.status(HTTP_STATUS.OK).json(updatedProfile);
     return;
   } catch (err) {
-    ServiceResponse.error(res, err);
-    return;
+    return next(err);
   }
 };
 
-const deleteProfile = function (req, res) {
+const deleteProfile = function (req, res, next) {
   try {
     res.status(HTTP_STATUS.OK).end();
     return;
   } catch (err) {
-    ServiceResponse.error(res, err);
-    return;
+    return next(err);
   }
 };
 
-const getProfile = function (req, res) {
+const getProfile = function (req, res, next) {
   try {
     res.status(HTTP_STATUS.OK).end();
     return;
   } catch (err) {
-    ServiceResponse.error(res, err);
-    return;
+    return next(err); 
   }
 };
 
