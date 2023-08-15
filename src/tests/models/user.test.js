36cefdcd3f92d641bbf1b6df30c82f9c2bb2870b
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-08-15 17:11:42 +0500

    fixed linting

--- src/tests/models/user.test.js
+++ src/tests/models/user.test.js
@@ -1,11 +1,11 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const { faker } = require("@faker-js/faker");
-const ERROR_TEXT = require('../../utils/constants/errorText');
-const UserFixtures = require('../fixtures/user');
-const UserModel = require('../../app/models').User;
+const ERROR_TEXT = require("../../utils/constants/errorText");
+const UserFixtures = require("../fixtures/user");
+const UserModel = require("../../app/models").User;
 
-describe('User Model', function(){
-  before(function() {
+describe("User Model", function () {
+  before(function () {
     this.payload = {
       firstName: faker.name.firstName(),
       lastName: faker.name.firstName(),
@@ -13,22 +13,22 @@ describe('User Model', function(){
       hashedPassword: faker.internet.password(),
     };
   });
-  describe('Create New User', function(){
-    it('should throw error if payload is not object', async function() {
-      try{
-        await UserModel.new('');
-      } catch({message}) {
-        expect(message).to.equal(ERROR_TEXT.NOT_OBJECT)
+  describe("Create New User", function () {
+    it("should throw error if payload is not object", async function () {
+      try {
+        await UserModel.new("");
+      } catch ({ message }) {
+        expect(message).to.equal(ERROR_TEXT.NOT_OBJECT);
       }
     });
-    it('should throw error if payload is empty object', async function() {
-      try{
+    it("should throw error if payload is empty object", async function () {
+      try {
         await UserModel.new({});
-      } catch({message}) {
-        expect(message).to.equal(ERROR_TEXT.EMPTY)
+      } catch ({ message }) {
+        expect(message).to.equal(ERROR_TEXT.EMPTY);
       }
     });
-    it('should create new user', async function() {
+    it("should create new user", async function () {
       await UserModel.new(this.payload);
       const newUser = await UserFixtures.getLatestCreatedUser();
       expect(newUser.firstName).equal(this.payload.firstName);
@@ -36,216 +36,218 @@ describe('User Model', function(){
       expect(newUser.username).equal(this.payload.username);
     });
   });
-  describe('Modify User', function(){
-    beforeEach(async function() {
-      this.user = await UserFixtures.createUser()
+  describe("Modify User", function () {
+    beforeEach(async function () {
+      this.user = await UserFixtures.createUser();
     });
-    it('should throw error if payload is not object', async function() {
-      try{
-        await UserModel.modify('', this.user.id);
-      } catch({message}) {
+    it("should throw error if payload is not object", async function () {
+      try {
+        await UserModel.modify("", this.user.id);
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.NOT_OBJECT);
       }
     });
-    it('should throw error if payload is empty object', async function() {
-      try{
+    it("should throw error if payload is empty object", async function () {
+      try {
         await UserModel.modify({}, this.user.id);
-      } catch({message}) {
-        expect(message).to.equal(ERROR_TEXT.EMPTY)
+      } catch ({ message }) {
+        expect(message).to.equal(ERROR_TEXT.EMPTY);
       }
     });
-    it('should throw error if user is undefined', async function() {
-      try{
+    it("should throw error if user is undefined", async function () {
+      try {
         await UserModel.modify(this.payload, undefined);
-      } catch({message}) {
-        expect(message).to.equal(ERROR_TEXT.INVALID_NUM)
+      } catch ({ message }) {
+        expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if user is null', async function() {
-      try{
+    it("should throw error if user is null", async function () {
+      try {
         await UserModel.modify(this.payload, null);
-      } catch({message}) {
-        expect(message).to.equal(ERROR_TEXT.INVALID_NUM)
+      } catch ({ message }) {
+        expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if user is 0', async function() {
-      try{
+    it("should throw error if user is 0", async function () {
+      try {
         await UserModel.modify(this.payload, 0);
-      } catch({message}) {
-        expect(message).to.equal(ERROR_TEXT.INVALID_NUM)
+      } catch ({ message }) {
+        expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if user is -ve', async function() {
-      try{
+    it("should throw error if user is -ve", async function () {
+      try {
         await UserModel.modify(this.payload, -10);
-      } catch({message}) {
-        expect(message).to.equal(ERROR_TEXT.INVALID_NUM)
+      } catch ({ message }) {
+        expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should update user details', async function() {
-      await UserModel.modify({firstName: 'newName'}, this.user.id);
+    it("should update user details", async function () {
+      await UserModel.modify({ firstName: "newName" }, this.user.id);
       const newUser = await UserFixtures.findUserById(this.user.id);
-      expect(newUser.firstName).equal('newName');
+      expect(newUser.firstName).equal("newName");
     });
   });
-  describe('FindLatest User', function(){
-    before(async function(){
+  describe("FindLatest User", function () {
+    before(async function () {
       this.user1 = await UserFixtures.createUser();
       this.user2 = await UserFixtures.createUser();
     });
-    it('should throw error if limit is undefined', async function(){
+    it("should throw error if limit is undefined", async function () {
       try {
-        await UserModel.findLatest(undefined, {})
-      } catch({message}) {
+        await UserModel.findLatest(undefined, {});
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if limit is 0', async function(){
+    it("should throw error if limit is 0", async function () {
       try {
-        await UserModel.findLatest(0, {})
-      } catch({message}) {
+        await UserModel.findLatest(0, {});
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if limit is null', async function(){
+    it("should throw error if limit is null", async function () {
       try {
-        await UserModel.findLatest(null, {})
-      } catch({message}) {
+        await UserModel.findLatest(null, {});
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if limit is -ve', async function(){
+    it("should throw error if limit is -ve", async function () {
       try {
-        await UserModel.findLatest(-1, {})
-      } catch({message}) {
+        await UserModel.findLatest(-1, {});
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if where is not a object', async function(){
+    it("should throw error if where is not a object", async function () {
       try {
-        await UserModel.findLatest(1, '')
-      } catch({message}) {
+        await UserModel.findLatest(1, "");
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.NOT_OBJECT);
       }
     });
-    it('should return latest user by default', async function(){
+    it("should return latest user by default", async function () {
       const user = await UserModel.findLatest();
-      expect(user).to.be.a('object');
+      expect(user).to.be.a("object");
       expect(user.id).to.equal(this.user2.id);
     });
-    it.skip('should return latest 2 user', async function(){
+    it.skip("should return latest 2 user", async function () {
       //TODO: need to fix model method
       const user = await UserModel.findLatest(2);
       console.log(user);
-      expect(user).to.be.an('array').that.have.lengthOf(2);
+      expect(user).to.be.an("array").that.have.lengthOf(2);
       expect(user[0].id).to.equal(this.user2.id);
       expect(user[1].id).to.equal(this.user1.id);
     });
   });
-  describe('findByUsername', function(){
-    beforeEach(async function() {
-      this.fakeUsername = faker.internet.userName(),
-      this.user1 = await UserFixtures.createUser({username: this.fakeUsername});
+  describe("findByUsername", function () {
+    beforeEach(async function () {
+      (this.fakeUsername = faker.internet.userName()),
+        (this.user1 = await UserFixtures.createUser({
+          username: this.fakeUsername,
+        }));
       this.user2 = await UserFixtures.createUser();
     });
-    it('should throw error if username is undefined', async function() {
+    it("should throw error if username is undefined", async function () {
       try {
         const user = await UserModel.findByUsername(undefined);
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.EMPTY);
       }
     });
-    it('should throw error if username is null', async function() {
+    it("should throw error if username is null", async function () {
       try {
         const user = await UserModel.findByUsername(null);
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.EMPTY);
       }
     });
-    it('should throw error if username is empty', async function() {
+    it("should throw error if username is empty", async function () {
       try {
         const user = await UserModel.findByUsername("");
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.EMPTY);
       }
     });
-    it('should find user by username', async function() {
+    it("should find user by username", async function () {
       const user = await UserModel.findByUsername(this.fakeUsername);
-      expect(user).to.be.a('object');
+      expect(user).to.be.a("object");
       expect(user.id).to.equal(this.user1.id);
     });
   });
-  describe('findById', function() {
-    beforeEach(async function() {
+  describe("findById", function () {
+    beforeEach(async function () {
       this.user = await UserFixtures.createUser();
     });
-    it('should throw error if id is undefined', async function() {
+    it("should throw error if id is undefined", async function () {
       try {
         const user = await UserModel.findById(undefined);
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if id is null', async function() {
+    it("should throw error if id is null", async function () {
       try {
         const user = await UserModel.findById(null);
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if id is 0', async function() {
+    it("should throw error if id is 0", async function () {
       try {
         const user = await UserModel.findById(0);
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if id is -ve', async function() {
+    it("should throw error if id is -ve", async function () {
       try {
         const user = await UserModel.findById(-1);
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should return user by its id', async function() {
+    it("should return user by its id", async function () {
       const user = await UserModel.findById(this.user.id);
-      expect(user).to.be.a('object');
+      expect(user).to.be.a("object");
       expect(user.id).to.equal(this.user.id);
     });
   });
-  describe('delete user', function() {
-    beforeEach(async function() {
+  describe("delete user", function () {
+    beforeEach(async function () {
       this.user = await UserFixtures.createUser();
     });
-    it('should throw error if id is undefined', async function() {
+    it("should throw error if id is undefined", async function () {
       try {
         const user = await UserModel.remove(undefined);
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if id is null', async function() {
+    it("should throw error if id is null", async function () {
       try {
         const user = await UserModel.remove(null);
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if id is 0', async function() {
+    it("should throw error if id is 0", async function () {
       try {
         const user = await UserModel.remove(0);
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should throw error if id is -ve', async function() {
+    it("should throw error if id is -ve", async function () {
       try {
         const user = await UserModel.remove(-1);
-      } catch({message}) {
+      } catch ({ message }) {
         expect(message).to.equal(ERROR_TEXT.INVALID_NUM);
       }
     });
-    it('should return user by its id', async function() {
+    it("should return user by its id", async function () {
       await UserModel.remove(this.user.id);
       const user = await UserFixtures.findUserById(this.user.id);
       expect(user).to.be.null;
