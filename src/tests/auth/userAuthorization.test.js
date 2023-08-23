5eb1fd5de1d88266bb19d65436a9ef077aaf5b56
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-08-23 13:30:01 +0500

    added test cases to cover missing coverage

--- src/tests/auth/userAuthorization.test.js
+++ src/tests/auth/userAuthorization.test.js
@@ -3,6 +3,7 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const HTTP_STATUS = require("../../utils/constants/httpStatus");
 const UserFixtures = require("../fixtures/user");
+const {expect} = chai;
 
 chai.use(chaiHttp);
 chai.should();
@@ -17,6 +18,23 @@ describe("Auth", function () {
         .set({ Authorization: `Bearer ${token}` })
         .send({});
       resp.status.should.equal(HTTP_STATUS.UNAUTHORIZED);
+      resp.body.should.empty;
+    });
+    it("should throw error if auth header not found", async function () {
+      const resp = await request(app)
+        .get("/api/v1/")
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.UNAUTHORIZED);
+      expect(resp.text).to.equal('token not found')
+      resp.body.should.empty;
+    });
+    it("should throw error if auth token not found", async function () {
+      const resp = await request(app)
+        .get("/api/v1/")
+        .set({ Authorization: `Bearer` })
+        .send({});
+      resp.status.should.equal(HTTP_STATUS.UNAUTHORIZED);
+      expect(resp.text).to.equal('token is empty')
       resp.body.should.empty;
     });
   });
